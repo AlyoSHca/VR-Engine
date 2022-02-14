343dbae81a54b9a5bddf5afc10978b77c59069a1
AlyoSHca <alyoshcasyryh@mail.ru>
2022-02-15 00:59:46 +0300

    Добавление интерфейса для редактора карт Terrain_editor

---
+++ Программа/terrain_editor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visualizer
+{
+    public partial class terrain_editor : Form
+    {
+        Relief_map rm = new Relief_map();
+        PaintTexture paintTexture = new PaintTexture();
+        Generation_rule gr = new Generation_rule();
+        public terrain_editor()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
