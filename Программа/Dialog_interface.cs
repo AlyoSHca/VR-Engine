98a447c690578f4d6b4ad69d465a6478dbe7beb8
AlyoSHca <alyoshcasyryh@mail.ru>
2022-02-15 00:25:39 +0300

    Добавление интерфейса для создания и редактирования диалоговых окон Dialog_interface

---
+++ Программа/Dialog_interface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visualizer
+{
+    public partial class Dialog_interface : Form
+    {
+        DialogEditor editor = new DialogEditor();
+        Pattern_dialog pattern = new Pattern_dialog();
+        public Dialog_interface()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
